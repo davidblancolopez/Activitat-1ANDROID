2794b26778528b36cfe5671f16e49124b87dfc79
davidblancolopez <daviddbl94@gmail.com>
2016-10-26 23:59:36 +0200

    Cambios

--- app/src/main/java/com/example/david/activitat_1/MainActivity.java
+++ app/src/main/java/com/example/david/activitat_1/MainActivity.java
@@ -1,20 +1,51 @@
 package com.example.david.activitat_1;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, DatePickerDialog.OnDateSetListener {
+
+    private EditText fromDateEtxt;
+    private DatePickerDialog fromDatePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+        //Inicializamos el boton
         Button buttonEnviar = (Button) findViewById(R.id.buttonEnviar);
+        //Le aplicamos un clickListener
         buttonEnviar.setOnClickListener(this);
+
+
+
+
+        /* Inicializamos el EditText y pedimos el focus */
+        fromDateEtxt = (EditText) findViewById(R.id.etFecha);
+           /* fromDateEtxt.setInputType(InputType.TYPE_NULL);
+            fromDateEtxt.requestFocus(); */
+        fromDateEtxt.setFocusable(false);
+
+            /* Ponemos el Listener al EditText */
+        fromDateEtxt.setOnClickListener(this);
+            /* Creamos el DatePickerDialog a partir de la fechaActual */
+        Calendar newCalendar = Calendar.getInstance();
+        fromDatePickerDialog = new DatePickerDialog(this, this, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+            /* El constructor del DatePickerDialog accepta per paràmetres
+               1. El context (o l'Activity on és llençat)
+               2. L'objecte que el "vigila" i que implementa la interfície onDateSetListener
+               3. Any, mes i dia del calendar que es mostren per defecte
+             */
     }
 
 
@@ -29,4 +60,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
+    @Override
+    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+
+    }
 }
